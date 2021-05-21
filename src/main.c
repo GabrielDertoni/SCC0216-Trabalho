2bff1110a311e16d04668cfa405aae27f431adfc
Gabriel Dertoni <gab.dertoni@gmail.com>
2021-05-21 19:44:30 -0300

    Fix read_word

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include <bin.h>
 #include <external.h>
 
+#include <errno.h>
+
 // #include<sql.h>
 
 #define QUOTE 34
@@ -18,21 +20,21 @@
 #define BUFFER 4096
 #define and &&
 
-char *LerPalavraSemCitacao(FILE*);
+char *read_word(FILE *in);
 
 int main(void){
     int operacao;
     scanf("%d", &operacao);
     fgetc(stdin); // Consome o espaço (32) logo após o inteiro
-    char *file_name = LerPalavraSemCitacao(stdin), *input1 = NULL, *input2 = NULL;
+    char *file_name = read_word(stdin), *input1 = NULL, *input2 = NULL;
     switch(operacao){
         case 1:
-            input1 = LerPalavraSemCitacao(stdin);
+            input1 = read_word(stdin);
             if(CREATE_TABLE_FILE(file_name, input1))
                 binarioNaTela(input1);
             break;
         case 2:
-            input1 = LerPalavraSemCitacao(stdin);
+            input1 = read_word(stdin);
             CREATE_TABLE_LINE(file_name, input1);
             binarioNaTela(input1);
             break; 
@@ -43,22 +45,22 @@ int main(void){
             SELECT_FROM_WHERE_LINE(file_name, NULL, NULL);
             break;
         case 5:
-            input1 = LerPalavraSemCitacao(stdin);
-            input2 = LerPalavraSemCitacao(stdin);
+            input1 = read_word(stdin);
+            input2 = read_word(stdin);
             SELECT_FROM_WHERE_FILE(file_name, input1, input2);
             break;
         case 6:
-            input1 = LerPalavraSemCitacao(stdin);
-            input2 = LerPalavraSemCitacao(stdin);
+            input1 = read_word(stdin);
+            input2 = read_word(stdin);
             SELECT_FROM_WHERE_LINE(file_name, input1, input2);
             break;
         case 7:
-            input1 = LerPalavraSemCitacao(stdin);
+            input1 = read_word(stdin);
             if(INSERT_INTO_FILE(file_name))
                 binarioNaTela(file_name);
             break;
         case 8:
-            input1 = LerPalavraSemCitacao(stdin);
+            input1 = read_word(stdin);
             if(INSERT_INTO_LINE(file_name))
                 binarioNaTela(file_name);
             break;
@@ -73,21 +75,23 @@ int main(void){
     return 0;
 }
 
-char *LerPalavraSemCitacao(FILE *in){
+char *read_word(FILE *in) {
     char *string = NULL;
-    long start, end;
+	size_t len = 0;
     int character;
-    start = end = ftell(in);
-    do{
-        if(end-start % BUFFER == 0)
-            string = realloc(string, (end-start / BUFFER + 1) * BUFFER);
+
+    do {
+        if(len % BUFFER == 0)
+            string = realloc(string, (len / BUFFER + 1) * BUFFER + 1);
+
         character = fgetc(in);
-        if(character != CARRIAGE_RETURN and character != QUOTE){
-            string[end-start] = character;
-            end++;
-        }
-    }while(character != SPACE and character != BREAK_LINE and character != EOF);
-    string[end-start-1] = 0;
-    string = realloc(string, end-start);
+
+        if(character != CARRIAGE_RETURN and character != QUOTE)
+            string[len++] = character;
+
+    } while (character != SPACE and character != BREAK_LINE and character != EOF);
+
+    string[len-1] = '\0';
+    string = realloc(string, len);
     return string;
 }
