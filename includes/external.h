df4513b73157cf6e0c8c4864c8bce013bb75b501
GabrielDertoni <gab.dertoni@usp.br>
2021-06-27 02:42:27 -0300

    Add comments to external functions header

--- includes/external.h
+++ includes/external.h
@@ -6,6 +6,19 @@
 */
 void binarioNaTela(char *nomeArquivoBinario);
 
+
+/*
+*    Use essa função para ler um campo string delimitado entre aspas (").
+*    Chame ela na hora que for ler tal campo. Por exemplo:
+*
+*    A entrada está da seguinte forma:
+*        nomeDoCampo "MARIA DA SILVA"
+*
+*    Para ler isso para as strings já alocadas str1 e str2 do seu programa, você faz:
+*        scanf("%s", str1); // Vai salvar nomeDoCampo em str1
+*        scan_quote_string(str2); // Vai salvar MARIA DA SILVA em str2 (sem as aspas)
+*
+*/
 void scan_quote_string(char *str);
 
 /*  
